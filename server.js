77542045a7f5576ce67c7cb8583410c193c7e830
Gal Kol <999galk@gmail.com>
2020-02-14 00:50:30 +0200

    added saveUninitialized: true,

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(session({
   store: new (require('connect-pg-simple')(session))(),
   secret: process.env.FOO_COOKIE_SECRET,
   resave: false,
+  saveUninitialized: true,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
 }));
 app.use(cors());
