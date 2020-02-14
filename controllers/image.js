4ca2c6bdfd307889757d5f9dc850db3e456802df
Gal Kol <999galk@gmail.com>
2020-02-14 10:28:21 +0200

    changed endpoint to call api

--- controllers/image.js
+++ controllers/image.js
@@ -4,19 +4,25 @@ const app = new Clarifai.App({
  apiKey: '2c8dcc1c39c242d284f70ebbb9584cdb'
 });
 
-const handleApiCall = () => (req,res) => {
+const handleApiCall = (url, id, sessionData) => {
 	app.models
-	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-	.then(data => res.json(data))
-	.catch(err => res.status(400).json('Error getting clarifai'));
+	.predict(Clarifai.FACE_DETECT_MODEL, url)
+	.then(data => {
+		sessionData.status = 'completed';
+		console.log('session status:', sessionData.status);
+		res.json(data, id);
+	}).catch(err => res.status(400).json('Error getting clarifai'));
 }
 
 const changeEntries = (db) => (req,res) => {
 	const { input } = req.body;
-	
+	const sessionData = req.session;
+
 	db('entries').returning('id').insert({url : input}).count('id as CNT').then(id => {
     if(id.length){
-    	res.json(id);
+    	sessionData.status = 'saved_successfully';
+    	console.log('session status:', sessionData.status);
+    	handleApiCall(input, id, sessionData);
     }else{
     	res.status(404).json('user doesnt exist');
     }
