3fae52f198aacd4091188a40b6b858a16781eea8
Gal Kol <999galk@gmail.com>
2020-02-15 22:13:09 +0200

    changed cors conf3

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 app.use(session({
